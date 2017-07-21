a5ae27319f01773b340bbec4a0ac9d083de0d4fd
Gora95 <alejandrogori95@gmail.com>
2017-07-21 01:56:28 -0500

    Modelos y gitignore
    Agrega el modelo de buildingimages y complementa el de building.
    Se agrega el gitignore :)

---
+++ app/Models/BuildingImages.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BuildingImages extends Model
+{
+    
+    protected $table = 'building_images';
+
+    protected $fillable = ['building_id', 'path'];
+
+    public function building(){
+    	$this->belongsTo('App\Models\Building');
+    }
+
+}
--- app/Models/JiBuilding.php
+++ app/Models/JiBuilding.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class JiBuilding extends Model
 {
-    //
+    
+    protected $table = 'building';
+
+    protected $fillable = ['land_id','warehouse_id','office_id','house_id','extra_data'];
+    	
+
+    public function images(){
+    	return $this->hasMany('App\Models\building_images');
+    }
 }
