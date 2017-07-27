b219fea973e1d3fc093a992d5040aefebff6a0aa
Gora95 <alejandrogori95@gmail.com>
2017-07-27 11:22:16 -0500

    Modificaciones
    Se agrupan las rutas dentro del cors middleware y dentro la api de
    auth, quitar el prefijo para producción.

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -22,12 +22,4 @@ class CorsMiddleware
         return $next($request);
     }
 
-    /*public function terminate($request, $response)
-    {
-        return $response->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, token');
-    }*/
-
-
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,34 +16,54 @@ Route::get('/', function () {
 });
 
 
-Route::post('authenticate', 'AuthenticateController@authenticate');
-Route::group(['middleware' => ['jwt.auth', 'cors']], function(){
+Route::group(['middleware' => ['cors'], 'prefix'=>'api'], function(){
+	Route::post('authenticate', 'AuthenticateController@authenticate');
 
-	Route::post('register', 'AuthenticateController@register');
-	Route::get('user/data', 'AuthenticateController@getUserData');
-	Route::get('user/menu', 'UserController@getMenu');
-	Route::get('user/{id}/branch', 'UserController@getBranch');
-	Route::get('user/kind/{tipo}', 'UserController@getTipo');
-	Route::get('user/kinds', 'UserController@kinds');
-	Route::put('user/change-password/{id?}', 'UserController@changePassword');
-	Route::resource('user', 'UserController', ['except' => ['create', 'store']]);
+	Route::group(['middleware' => ['jwt.auth']], function(){
+		Route::post('register', 'AuthenticateController@register');
+		Route::get('user/data', 'AuthenticateController@getUserData');
+		Route::get('user/menu', 'UserController@getMenu');
+		Route::get('user/{id}/branch', 'UserController@getBranch');
+		Route::get('user/kind/{tipo}', 'UserController@getTipo');
+		Route::get('user/kinds', 'UserController@kinds');
+		Route::put('user/change-password/{id?}', 'UserController@changePassword');
+		Route::resource('user', 'UserController', ['except' => ['create', 'store']]);
 
-	Route::get('branch/{id}/users', 'BranchController@users');
-	Route::resource('branch', 'BranchController', ['except' => ['create']]);
+		Route::get('branch/{id}/users', 'BranchController@users');
+		Route::resource('branch', 'BranchController', ['except' => ['create']]);
 
-	Route::post('building/upload/{id?}', 'ImageController@upload');
-	Route::resource('building', 'BuildingController', ['except' => ['create']]);
+		Route::post('building/upload/{id?}', 'ImageController@upload');
+		Route::resource('building', 'BuildingController', ['except' => ['create']]);
 
-	Route::get('customer/options/{option}', 'CustomerController@options');
-	Route::resource('customer', 'CustomerController', ['except' => ['index', 'create']]);
-	Route::resource('location', 'LocationController', ['except' => ['index', 'create']]);
-	Route::resource('land', 'LandController', ['except' => ['index', 'create']]);
-	Route::resource('warehouse', 'WarehouseController', ['except' => ['index', 'create']]);
-	Route::resource('office', 'OfficeController', ['except' => ['index', 'create']]);
-	Route::resource('housing', 'HousingController', ['except' => ['index', 'create']]);
-	Route::resource('prospect', 'ProspectController', ['except' => ['index', 'create']]);
-	Route::resource('date', 'DateController', ['except' => ['index', 'create']]);
-	Route::resource('sale', 'SaleController', ['except' => ['index', 'create']]);
-	Route::resource('document', 'DocumentController', ['except' => ['index', 'create']]);
-	Route::resource('phone', 'PhoneController', ['except' => ['index', 'create']]);
+		Route::get('customer/options/{option}', 'CustomerController@options');
+		Route::resource('customer', 'CustomerController', ['except' => ['index', 'create']]);
+		Route::resource('location', 'LocationController', ['except' => ['index', 'create']]);
+		Route::resource('land', 'LandController', ['except' => ['index', 'create']]);
+		Route::resource('warehouse', 'WarehouseController', ['except' => ['index', 'create']]);
+		Route::resource('office', 'OfficeController', ['except' => ['index', 'create']]);
+		Route::resource('housing', 'HousingController', ['except' => ['index', 'create']]);
+		Route::resource('prospect', 'ProspectController', ['except' => ['index', 'create']]);
+		Route::resource('date', 'DateController', ['except' => ['index', 'create']]);
+		Route::resource('sale', 'SaleController', ['except' => ['index', 'create']]);
+		Route::resource('document', 'DocumentController', ['except' => ['index', 'create']]);
+		Route::resource('phone', 'PhoneController', ['except' => ['index', 'create']]);
+	});
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
